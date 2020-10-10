277fb3e5cd45ea5461e1572441e304c8e5e4efd0
Paul Clark <pdclark@pdclark.com>
2020-10-10 16:10:34 -0700

    Add random_int fallback

--- web/index.php
+++ web/index.php
@@ -2,6 +2,53 @@
 
 require_once dirname(__DIR__) . '/lib/Yijing.php';
 
+if (!function_exists('random_int')) {
+    function random_int($min, $max) {
+        if (!function_exists('mcrypt_create_iv')) {
+            trigger_error(
+                'mcrypt must be loaded for random_int to work', 
+                E_USER_WARNING
+            );
+            return null;
+        }
+        
+        if (!is_int($min) || !is_int($max)) {
+            trigger_error('$min and $max must be integer values', E_USER_NOTICE);
+            $min = (int)$min;
+            $max = (int)$max;
+        }
+        
+        if ($min > $max) {
+            trigger_error('$max can\'t be lesser than $min', E_USER_WARNING);
+            return null;
+        }
+        
+        $range = $counter = $max - $min;
+        $bits = 1;
+        
+        while ($counter >>= 1) {
+            ++$bits;
+        }
+        
+        $bytes = (int)max(ceil($bits/8), 1);
+        $bitmask = pow(2, $bits) - 1;
+
+        if ($bitmask >= PHP_INT_MAX) {
+            $bitmask = PHP_INT_MAX;
+        }
+
+        do {
+            $result = hexdec(
+                bin2hex(
+                    mcrypt_create_iv($bytes, MCRYPT_DEV_URANDOM)
+                )
+            ) & $bitmask;
+        } while ($result > $range);
+
+        return $result + $min;
+    }
+}
+
 class api {
 	
 	public $values = [
@@ -181,7 +228,14 @@ if ( isset( $_GET['roll'] ) ) {
 </head>
 
 <body>
+	<pre><a href="javascript:window.location.reload()">( Roll )</a></pre>
 <div id="app"></div>
+
+
+<pre>
+
+</pre>
+
 <script   src="https://code.jquery.com/jquery-3.5.1.min.js"   integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="   crossorigin="anonymous"></script>
 <script>
 jQuery(document).ready( function($){
