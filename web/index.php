f640444594ea882d44600b24268129d5e7c0190a
Paul Clark <pdclark@pdclark.com>
2020-10-19 17:54:27 -0700

    body overflow-x

--- web/index.php
+++ web/index.php
@@ -137,7 +137,7 @@
 			}
 		}
 
-		body{ margin-left: 22vw; top:0; width: 80vw; font-size: 2vw; background: #fff ; font-size: 2.8vw; overflow:hidden;}
+		body{ margin-left: 22vw; top:0; width: 80vw; font-size: 2vw; background: #fff ; font-size: 2.8vw; overflow-x:hidden;}
 	</style>
 	<script   src="https://code.jquery.com/jquery-3.5.1.min.js"   integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="   crossorigin="anonymous"></script>
 	</head>
